3f33b9bafd4ceadb7912b1fd358dd33b617f9b47
Fablaney <fabrmoto@gmail.com>
2022-10-17 11:28:17 +0200

    opti features

--- front-end/src/api/data_mock.js
+++ front-end/src/api/data_mock.js
@@ -1,13 +1,16 @@
 const accueil_data = [
     {
+        type: "chat",
         title: "You are our #1 priority",
         text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
     },
     {
+        type: "money",
         title: "More savings means higher rates",
         text: "The more you save with us, the higher your interest rate will be!",
     },
     {
+        type: "security",
         title: "Security you can trust",
         text: "We use top of the line encryption to make sure your data and money is always safe.",
     },
--- front-end/src/components/Sectionfeatures/index.jsx
+++ front-end/src/components/Sectionfeatures/index.jsx
@@ -1,5 +1,7 @@
 import { accueil_data } from '../../api/data_mock'
 import chaticon from "../../designs/img/icon-chat.png"
+import money from "../../designs/img/icon-money.png"
+import security from "../../designs/img/icon-security.png"
 
 function SectionFeatures()
 {
@@ -10,10 +12,26 @@ function SectionFeatures()
 
             {
                 accueil_data.map((item) => {
+
+                    let img
+
+                    if(item.type == "chat")
+                    {
+                        img = <img src={chaticon} alt="Chat Icon" className="feature-icon" />
+                    }
+                    if(item.type == "money")
+                    {
+                        img = <img src={money} alt="Money Icon" className="feature-icon" />
+                    }
+                    if(item.type == "security")
+                    {
+                        img = <img src={security} alt="Security Icon" className="feature-icon" />
+                    }
+
                     return (
                         <div className="feature-item" key={item.title}>
-
-                            <img src={chaticon} alt="Chat Icon" className="feature-icon" />
+                            
+                            {img}
                 
                             <h3 className="feature-item-title">{item.title}</h3>
                 
